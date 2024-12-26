79909b49ac73b1b96098ad99f9808c5658927918
İlyas Torun <ilyastorunn@outlook.com>
2024-12-26 03:39:18 +0300

    minor changes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "../styles/Slider.css";
 
-// Import images
 import album1 from "../../public/pics/album1.jpg";
 import album2 from "../../public/pics/album2.jpg";
 import album3 from "../../public/pics/album3.jpg";
@@ -94,5 +93,70 @@ const Slider = () => {
     </div>
   );
 };
+//   <div className="carousel-container h-screen flex flex-col justify-center items-center relative overflow-hidden">
+//     <div id="carousel" className="relative h-[400px] w-4/5 flex justify-center items-center mx-auto">
+//       {images.map((img, index) => {
+//         const className = getClassName(index)
+//         return (
+//           <div
+//             key={index}
+//             className={`absolute transition-all duration-500 ${className}`}
+//             onClick={() => setSelectedIndex(index)}
+//           >
+//             <div className="relative">
+//               {className === 'selected' && (
+//                 <div 
+//                   className={`vinyl absolute left-1/2 -translate-x-1/2 top-[25px] w-[300px] h-[300px] rounded-full overflow-hidden z-0 transition-all duration-1000 ease-in-out
+//                     ${vinylVisible ? 'vinyl-visible' : 'vinyl-hidden'}`}
+//                 >
+//                   <Image
+//                     src={img.src}
+//                     alt={`Vinyl for ${img.albumName} by ${img.artist}`}
+//                     layout="fill"
+//                     objectFit="cover"
+//                     className="vinyl-image"
+//                   />
+//                   <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[80px] h-[80px] rounded-full bg-black">
+//                     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[15px] h-[15px] rounded-full bg-gray-600" />
+//                   </div>
+//                 </div>
+//               )}
+//               <Image
+//                 src={img.src}
+//                 alt={`Album cover for ${img.albumName} by ${img.artist}`}
+//                 width={350}
+//                 height={350}
+//                 className="rounded-lg shadow-lg transition-all duration-500 object-cover relative z-10"
+//               />
+//             </div>
+//             {className === 'selected' && (
+//               <div className="album-info absolute -bottom-16 left-0 w-full text-center">
+//                 <p className="artist text-lg font-semibold mb-1">{img.artist}</p>
+//                 <p className="album-name text-base text-gray-600">{img.albumName}</p>
+//               </div>
+//             )}
+//           </div>
+//         )
+//       })}
+//     </div>
+//     <div className="buttons absolute w-full top-1/2 -translate-y-1/2 flex justify-between px-5 box-border pointer-events-none z-20">
+//       <button
+//         className="prev-button text-white hover:text-orange-500 text-4xl border-none bg-transparent transition-all duration-300 hover:scale-125 pointer-events-auto"
+//         onClick={() => moveToSelected('prev')}
+//         aria-label="Previous image"
+//       >
+//         <ChevronLeft size={50} />
+//       </button>
+//       <button
+//         className="next-button text-white hover:text-orange-500 text-4xl border-none bg-transparent transition-all duration-300 hover:scale-125 pointer-events-auto"
+//         onClick={() => moveToSelected('next')}
+//         aria-label="Next image"
+//       >
+//         <ChevronRight size={50} />
+//       </button>
+//     </div>
+//   </div>
+// )
+// }
 
 export default Slider;
