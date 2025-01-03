4a8b02ef1bcfd5cebdac319010b69b4da16d42cd
İlyas Torun <ilyastorunn@outlook.com>
2025-01-03 21:46:53 +0300

    minor changes

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Player() {
 
   return (
-    <div className="relative">
+    <div className="flex justify-center items-center h-full">
       <div
-        className="absolute left-1/2 -translate-x-1/2 top-0 w-[200px] h-[200px] rounded-full overflow-hidden z-0 transition-all duration-1000 ease-in-out opacity-100 scale-100 -translate-y-[45%]"
+        className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[200px] h-[200px] rounded-full overflow-hidden z-0 transition-all duration-1000 ease-in-out opacity-100 scale-100"
       >
         <div className="w-full h-full relative animate-spin-slow">
           <img src="../../public/pics/album4.jpg" alt="spin" />
