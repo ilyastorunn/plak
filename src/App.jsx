acbae878efe0a3cc5af964dd1b6d2bb041da3051
İlyas Torun <ilyastorunn@outlook.com>
2025-01-11 18:02:20 +0300

    feat: add volume slider for Player page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import Albums from "./pages/Albums";
 import Player from "./pages/Player";
+import VolumeSlider from "./components/VolumeSlider";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/albums" element={<Albums />} />
-          <Route path="/player/:id" element={<Player />} />
-          {/* <Route path="/player" element={<Player />} /> */}
-        </Routes>
-      </BrowserRouter>
+    <VolumeSlider />
+      //<BrowserRouter>
+        //<Routes>
+          //<Route path="/" element={<LandingPage />} />
+          //<Route path="/albums" element={<Albums />} />
+          //<Route path="/player/:id" element={<Player />} />
+          //{/* <Route path="/player" element={<Player />} /> */}
+        //</Routes>
+      //</BrowserRouter>
    
   );
 }
